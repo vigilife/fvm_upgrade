3595fc6ecc7c2256bec6fe160bf387d5f3936490
Rexios <rexios@rexios.dev>
2023-09-19 07:53:18 -0400

    Add print statements for debugging

--- bin/fvm_upgrade.dart
+++ bin/fvm_upgrade.dart
@@ -18,16 +18,24 @@ void main() async {
   final latestRelease =
       releases.firstWhere((release) => release['hash'] == latest);
   final latestVersion = latestRelease['version'] as String;
+  print('Latest Flutter version: $latestVersion');
 
   final fvmConfigs = Directory.current
       .listSync(recursive: true)
       .whereType<File>()
       .where((e) => e.path.endsWith('fvm_config.json'));
 
+  print('Found ${fvmConfigs.length} fvm_config.json file(s):');
+  for (final config in fvmConfigs) {
+    print('  ${config.path}');
+  }
+
   for (final config in fvmConfigs) {
     final string = config.readAsStringSync();
     final json = jsonDecode(string) as Map<String, dynamic>;
     json['flutterSdkVersion'] = latestVersion;
     config.writeAsStringSync(const JsonEncoder.withIndent('  ').convert(json));
+
+    print('Updated ${config.path}');
   }
 }
